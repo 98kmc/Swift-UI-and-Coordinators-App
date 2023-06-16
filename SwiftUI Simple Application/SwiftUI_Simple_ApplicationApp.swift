35a20f44a56849d55e46a430c44ddac9347422c3
Miguel <centenokm.ing@gmail.com>
2023-06-16 08:39:09 -0700

    Initial Commit

---
+++ SwiftUI Simple Application/SwiftUI_Simple_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Simple_ApplicationApp.swift
+//  SwiftUI Simple Application
+//
+//  Created by Miguel on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Simple_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
